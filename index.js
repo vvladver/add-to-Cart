69f5550ad2e9f6ae092ef8c25eed6fdf8244b6e6
vvladver <vladislav.vereshchagin39@gmail.com>
2023-10-01 16:03:41 +0200

    Updated database, added product change function, redesigned the product

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove,set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-71549-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -13,63 +13,113 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+
+/* Create a new item and push it to the firebase */
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    
     if(inputValue == "" ){
-        alert("Insert an item, ill hunt it ")
+        alert(" Enter a new product ")
     }else{
         push(shoppingListInDB, inputValue)
         
     }
     
-    
     clearInputFieldEl()
 }
 )
 
+
+/* Updates a shopping list element in the HTML*/
 onValue(shoppingListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-        
     
         clearShoppingListEl()
         
         for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
+            let currentItem = itemsArray[i];
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1];
             
             appendItemToShoppingListEl(currentItem)
         }    
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+        shoppingListEl.innerHTML = "No items here... yet";
     }
 })
 
 function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
+    shoppingListEl.innerHTML = "";
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+    inputFieldEl.value = "";
 }
 
+
+/* Create new elements in HTML*/
 function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
+    let itemID = item[0];
+    let itemValue = item[1];
     
-    let newEl = document.createElement("li")
-    
+    let newEl = document.createElement("li");
+    let editIcon = document.createElement("i");
+    let deltBtn = document.createElement("i")
+
+    deltBtn.className = "fa-solid fa-xmark close-btn";
+    editIcon.className = "fa-solid fa-ellipsis change-btn";
+
     newEl.textContent = itemValue
+    newEl.appendChild(editIcon);
+    newEl.appendChild(deltBtn);
+
+    let editMode = false
     
-    newEl.addEventListener("click", function() {
+    deltBtn.addEventListener("click", function() {
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
         
         remove(exactLocationOfItemInDB)
     })
+
+editIcon.addEventListener("click",function(){
+
+    const previousValue = itemValue
     
+   if (!editMode ){
+    const inputField = document.createElement("input");
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Save";
+
+    newEl.appendChild(inputField);
+    newEl.appendChild(saveButton);
+    editMode = true 
+
+
+
+saveButton.addEventListener("click",function(){
+    let newValue = inputField.value 
+    if(newValue == ""){
+        newValue = previousValue
+        newEl.removeChild(inputField);
+        newEl.removeChild(saveButton);
+        editMode = false;
+    }else {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        set(exactLocationOfItemInDB,newValue)
+    }
+})
+   }else {
+    return ; 
+   }
+})
     shoppingListEl.append(newEl)
 }
+
+
+
+
+
+
+//Сделать так,чтобы оно подсчитывало, сколько сделано за сегодня и по истечению дня ресетнулось 
 
